c5c0f4211384b2a243d75565748c333751d7bf12
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-07-02 18:30:54 +0200

    ♻️ Move most type declarations to separate file

--- src/swarpc.js
+++ src/swarpc.js
@@ -1,41 +1,16 @@
 import { type } from "arktype"
-/**
- * @import { Type } from 'arktype';
- */
 
 /**
- * @template {Type} I
- * @template {Type} P
- * @template {Type} S
- * @typedef {Object} Procedure
- * @property {I} input
- * @property {P} progress
- * @property {S} success
- */
-
-/**
- * @template {Type} I
- * @template {Type} P
- * @template {Type} S
- * @typedef {(input: I['inferOut'], onProgress: (progress: P['inferOut']) => void) => Promise<NoInfer<S>['inferOut'] | NoInfer<S>['inferOut']>} ProcedureImplementation
- */
-
-/**
- * @typedef {Record<string, Procedure<Type, Type, Type>>} ProceduresMap
- */
-
-/**
- * @template {ProceduresMap} Procedures
- * @typedef {{ procedures: Procedures, implementations: {[F in keyof Procedures]: ProcedureImplementation<Procedures[F]['input'], Procedures[F]['progress'], Procedures[F]['success']> }, start: (self: Window) => void  } & { [F in keyof Procedures]: (impl: NoInfer<ProcedureImplementation<Procedures[F]['input'], Procedures[F]['progress'], Procedures[F]['success'] >>) => void }} SwarpServer
+ * @import { ProceduresMap, SwarpcClient, SwarpcServer } from './typings.js'
  */
 
 /**
  * @template {ProceduresMap} Procedures
  * @param {Procedures} procedures
- * @returns {SwarpServer<Procedures>}
+ * @returns {SwarpcServer<Procedures>}
  */
 export function Server(procedures) {
-  /** @type {SwarpServer<Procedures>}  */
+  /** @type {SwarpcServer<Procedures>}  */
   // @ts-expect-error
   const instance = {
     procedures,
@@ -115,22 +90,12 @@ export function Server(procedures) {
 }
 
 /**
- * @template {Procedure<Type, Type, Type>} P
- * @typedef {(input: P['input']['inferOut'], onProgress?: (progress: P['progress']['inferOut']) => void) => Promise<P['success']['inferOut']>} ClientMethod
- */
-
-/**
- * @template {ProceduresMap} Procedures
- * @typedef {{ procedures: Procedures } & { [F in keyof Procedures]: ClientMethod<Procedures[F]> }} SwarpClient
- */
-
-/**
  * @template {ProceduresMap} Procedures
  * @param {Procedures} procedures
- * @returns {SwarpClient<Procedures>}
+ * @returns {SwarpcClient<Procedures>}
  */
 export function Client(procedures) {
-  /** @type {SwarpClient<Procedures>} */
+  /** @type {SwarpcClient<Procedures>} */
   // @ts-expect-error
   const instance = { procedures }
 
---
+++ src/typings.js
@@ -0,0 +1,42 @@
+/**
+ * @import { Type } from 'arktype';
+ */
+
+/**
+ * @template {Type} I
+ * @template {Type} P
+ * @template {Type} S
+ * @typedef {Object} Procedure
+ * @property {I} input
+ * @property {P} progress
+ * @property {S} success
+ */
+
+/**
+ * @template {Type} I
+ * @template {Type} P
+ * @template {Type} S
+ * @typedef {(input: I['inferOut'], onProgress: (progress: P['inferOut']) => void) => Promise<NoInfer<S>['inferOut'] | NoInfer<S>['inferOut']>} ProcedureImplementation
+ */
+
+/**
+ * @typedef {Record<string, Procedure<Type, Type, Type>>} ProceduresMap
+ */
+
+/**
+ * @template {Procedure<Type, Type, Type>} P
+ * @typedef {(input: P['input']['inferOut'], onProgress?: (progress: P['progress']['inferOut']) => void) => Promise<P['success']['inferOut']>} ClientMethod
+ */
+
+/**
+ * @template {ProceduresMap} Procedures
+ * @typedef {{ procedures: Procedures } & { [F in keyof Procedures]: ClientMethod<Procedures[F]> }} SwarpcClient
+ */
+
+/**
+ * @template {ProceduresMap} Procedures
+ * @typedef {{ procedures: Procedures, implementations: {[F in keyof Procedures]: ProcedureImplementation<Procedures[F]['input'], Procedures[F]['progress'], Procedures[F]['success']> }, start: (self: Window) => void  } & { [F in keyof Procedures]: (impl: NoInfer<ProcedureImplementation<Procedures[F]['input'], Procedures[F]['progress'], Procedures[F]['success'] >>) => void }} SwarpcServer
+ */
+
+// Required, otherwise nothing can be imported from this file
+export {}
