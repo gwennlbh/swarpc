05a6c05eaf1c656a045205683de9008f97b88837
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-07-19 14:59:06 +0200

    💡 Document code, cleanup logger shorthands

--- src/swarpc.ts
+++ src/swarpc.ts
@@ -10,16 +10,27 @@ import {
 
 export type { ProceduresMap, SwarpcClient, SwarpcServer } from "./types.js"
 
+/**
+ * Creates a sw&rpc server instance.
+ * @param procedures procedures the server will implement
+ * @param param1 various options
+ * @param param1.worker if provided, the server will use this worker to post messages, instead of sending it to all clients
+ * @returns a SwarpcServer instance. Each property of the procedures map will be a method, that accepts a function implementing the procedure. There is also .start(), to be called after implementing all procedures.
+ */
 export function Server<Procedures extends ProceduresMap>(
   procedures: Procedures,
   { worker }: { worker?: Worker } = {}
 ): SwarpcServer<Procedures> {
+  // Initialize the instance.
+  // Procedures and implementations are stored on properties with symbol keys,
+  // to avoid any conflicts with procedure names, and also discourage direct access to them.
   const instance = {
     [zProcedures]: procedures,
     [zImplementations]: {} as ImplementationsMap<Procedures>,
     start: (self: Window) => {},
   } as SwarpcServer<Procedures>
 
+  // Set all implementation-setter methods
   for (const functionName in procedures) {
     instance[functionName] = ((implementation) => {
       if (!instance[zProcedures][functionName]) {
@@ -29,6 +40,7 @@ export function Server<Procedures extends ProceduresMap>(
     }) as SwarpcServer<Procedures>[typeof functionName]
   }
 
+  // Define payload schema for incoming messages
   const PayloadSchema = type.or(
     ...Object.entries(procedures).map(([functionName, { input }]) => ({
       functionName: type(`"${functionName}"`),
@@ -38,6 +50,7 @@ export function Server<Procedures extends ProceduresMap>(
   )
 
   instance.start = (self: Window) => {
+    // Used to post messages back to the client
     const postMessage = async (
       data: { functionName: string; requestId: string } & Partial<{
         result: any
@@ -54,13 +67,16 @@ export function Server<Procedures extends ProceduresMap>(
       }
     }
 
+    // Listen for messages from the client
     self.addEventListener("message", async (event: MessageEvent) => {
+      // Decode the payload
       const { functionName, requestId, input } = PayloadSchema.assert(
         event.data
       )
 
       l.server.debug(requestId, `Received request for ${functionName}`, input)
 
+      // Prepare a function to post errors back to the client
       const postError = async (error: any) =>
         postMessage({
           functionName,
@@ -70,20 +86,24 @@ export function Server<Procedures extends ProceduresMap>(
           },
         })
 
+      // Retrieve the implementation for the requested function
       const implementation = instance[zImplementations][functionName]
       if (!implementation) {
         await postError("No implementation found")
         return
       }
 
+      // Call the implementation with the input and a progress callback
       await implementation(input, async (progress: any) => {
         l.server.debug(requestId, `Progress for ${functionName}`, progress)
         await postMessage({ functionName, requestId, progress })
       })
+        // Send errors
         .catch(async (error: any) => {
           l.server.error(requestId, `Error in ${functionName}`, error)
           await postError(error)
         })
+        // Send results
         .then(async (result: any) => {
           l.server.debug(requestId, `Result for ${functionName}`, result)
           await postMessage({ functionName, requestId, result })
@@ -94,22 +114,38 @@ export function Server<Procedures extends ProceduresMap>(
   return instance
 }
 
+/**
+ * Generate a random request ID, used to identify requests between client and server.
+ * @returns a 6-character hexadecimal string
+ */
 function generateRequestId(): string {
   return Math.random().toString(16).substring(2, 8).toUpperCase()
 }
 
+/**
+ * Pending requests are stored in a map, where the key is the request ID.
+ * Each request has a set of handlers: resolve, reject, and onProgress.
+ * This allows having a single listener for the client, and having multiple in-flight calls to the same procedure.
+ */
+const pendingRequests = new Map<string, PendingRequest>()
 type PendingRequest = {
   reject: (err: Error) => void
   onProgress: (progress: any) => void
   resolve: (result: any) => void
 }
 
-const pendingRequests = new Map<string, PendingRequest>()
-
+// Have we started the client listener?
 let _clientListenerStarted = false
+
+/**
+ * Starts the client listener, which listens for messages from the sw&rpc server.
+ * @param worker if provided, the client will use this worker to listen for messages, instead of using the service worker
+ * @returns
+ */
 async function startClientListener(worker?: Worker) {
   if (_clientListenerStarted) return
 
+  // Get service worker registration if no worker is provided
   if (!worker) {
     const sw = await navigator.serviceWorker.ready
     if (!sw?.active) {
@@ -122,15 +158,21 @@ async function startClientListener(worker?: Worker) {
   }
 
   const w = worker ?? navigator.serviceWorker
+
+  // Start listening for messages
   l.client.debug("", "Starting client listener on", w)
   w.addEventListener("message", (event) => {
+    // Get the data from the event
+    // We don't use a arktype schema here, we trust the server to send valid data
     const { functionName, requestId, ...data } =
       (event as MessageEvent).data || {}
 
+    // Sanity check in case we somehow receive a message without requestId
     if (!requestId) {
       throw new Error("[SWARPC Client] Message received without requestId")
     }
 
+    // Get the associated pending request handlers
     const handlers = pendingRequests.get(requestId)
     if (!handlers) {
       throw new Error(
@@ -138,6 +180,8 @@ async function startClientListener(worker?: Worker) {
       )
     }
 
+    // React to the data received
+    // Unless it's a progress update, the request is finished, thus we can remove it from the pending requests
     if ("error" in data) {
       handlers.reject(new Error(data.error.message))
       pendingRequests.delete(requestId)
@@ -152,10 +196,18 @@ async function startClientListener(worker?: Worker) {
   _clientListenerStarted = true
 }
 
+/**
+ *
+ * @param procedures procedures the client will be able to call
+ * @param param1 various options
+ * @param param1.worker if provided, the client will use this worker to post messages.
+ * @returns a sw&rpc client instance. Each property of the procedures map will be a method, that accepts an input and an optional onProgress callback.
+ */
 export function Client<Procedures extends ProceduresMap>(
   procedures: Procedures,
   { worker }: { worker?: Worker } = {}
 ): SwarpcClient<Procedures> {
+  // Store procedures on a symbol key, to avoid conflicts with procedure names
   const instance = { [zProcedures]: procedures } as Partial<
     SwarpcClient<Procedures>
   >
@@ -169,11 +221,15 @@ export function Client<Procedures extends ProceduresMap>(
       )
     }
 
+    // Set the method on the instance
     // @ts-expect-error
     instance[functionName] = (async (input: unknown, onProgress = () => {}) => {
+      // Validate the input against the procedure's input schema
       procedures[functionName].input.assert(input)
+      // Ensure that we're listening for messages from the server
       await startClientListener(worker)
 
+      // If no worker is provided, we use the service worker
       const w =
         worker ?? (await navigator.serviceWorker.ready.then((r) => r.active))
 
@@ -187,8 +243,12 @@ export function Client<Procedures extends ProceduresMap>(
 
         const requestId = generateRequestId()
 
+        // Store promise handlers (as well as progress updates handler)
+        // so the client listener can resolve/reject the promise (and react to progress updates)
+        // when the server sends messages back
         pendingRequests.set(requestId, { resolve, onProgress, reject })
 
+        // Post the message to the server
         l.client.debug(requestId, `Requesting ${functionName} with`, input)
         w.postMessage({ functionName, input, requestId })
       })
@@ -198,29 +258,46 @@ export function Client<Procedures extends ProceduresMap>(
   return instance as SwarpcClient<Procedures>
 }
 
+/**
+ * Convenience shortcuts for logging.
+ */
 const l = {
   server: {
-    debug: (rqid: string | null, message: string, ...args: any[]) =>
-      log("debug", "server", rqid, message, ...args),
-    info: (rqid: string | null, message: string, ...args: any[]) =>
-      log("info", "server", rqid, message, ...args),
-    warn: (rqid: string | null, message: string, ...args: any[]) =>
-      log("warn", "server", rqid, message, ...args),
-    error: (rqid: string | null, message: string, ...args: any[]) =>
-      log("error", "server", rqid, message, ...args),
+    debug: logger("debug", "server"),
+    info: logger("info", "server"),
+    warn: logger("warn", "server"),
+    error: logger("error", "server"),
   },
   client: {
-    debug: (rqid: string | null, message: string, ...args: any[]) =>
-      log("debug", "client", rqid, message, ...args),
-    info: (rqid: string | null, message: string, ...args: any[]) =>
-      log("info", "client", rqid, message, ...args),
-    warn: (rqid: string | null, message: string, ...args: any[]) =>
-      log("warn", "client", rqid, message, ...args),
-    error: (rqid: string | null, message: string, ...args: any[]) =>
-      log("error", "client", rqid, message, ...args),
+    debug: logger("debug", "client"),
+    info: logger("info", "client"),
+    warn: logger("warn", "client"),
+    error: logger("error", "client"),
   },
 }
 
+/**
+ * Creates partially-applied logging functions given the first 2 args
+ * @param severity
+ * @param side
+ * @returns
+ */
+function logger(
+  severity: "debug" | "info" | "warn" | "error",
+  side: "server" | "client"
+) {
+  return (rqid: string | null, message: string, ...args: any[]) =>
+    log(severity, side, rqid, message, ...args)
+}
+
+/**
+ * Send log messages to the console, with a helpful prefix.
+ * @param severity
+ * @param side
+ * @param rqid request ID
+ * @param message
+ * @param args passed to console methods directly
+ */
 function log(
   severity: "debug" | "info" | "warn" | "error",
   side: "server" | "client",
