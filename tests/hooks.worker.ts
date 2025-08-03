d748b5c4c0f1714ace9695ce849c9ee3dfaffe59
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-08-03 03:09:21 +0200

    🔇 Set loglevel to warn for hooks tests' server

--- tests/hooks.worker.ts
+++ tests/hooks.worker.ts
@@ -3,13 +3,13 @@ import { procedures } from "./hooks.procedures.js"
 
 declare const self: Worker
 
-const server = Server(procedures, { worker: self })
+const server = Server(procedures, { worker: self, loglevel: "warn" })
 server.echo(async ({ value }) => value)
 server.add(async ({ a, b }) => a + b)
 server.divide(async ({ a, b }, onProgress) => {
   for (let i = 1; i <= 3; i++) {
-    await new Promise((r) => setTimeout(r, 10))
-    onProgress({ percent: Math.floor((i / 3) * 100) })
+    await new Promise((r) => setTimeout(r, 5))
+    onProgress({ percent: (i / 3) * 100 })
   }
   if (b === 0) throw new Error("Division by zero")
   return a / b
