e2196dc3cfc62500cfaee96f94eb0683e8cb4681
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-11-05 09:02:32 +0100

    🚧 Try keeping the lib under 10 kB

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@
  */
 
 import "./polyfills.js";
-export * from "./client.js";
-export * from "./server.js";
+export { Client } from "./client.js";
+export { Server } from "./server.js";
 export type { ProceduresMap, CancelablePromise } from "./types.js";
